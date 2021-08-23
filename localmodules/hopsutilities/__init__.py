bce5ebfda042d4e84729ee8e3357ad7721984bc1
fstwn <post@maxeschenbach.com>
2021-08-23 11:34:00 +0200

    update hsutil

--- localmodules/hopsutilities/__init__.py
+++ localmodules/hopsutilities/__init__.py
@@ -1,3 +1,5 @@
-from .hopsutilities import (np_array_to_rhino_transform, # NOQA401
+from .hopsutilities import (rhino_points_to_np_array, # NOQA401
                             rhino_mesh_to_np_arrays,
+                            np_array_to_rhino_transform,
+                            np_array_to_rhino_points,
                             )
--- localmodules/hopsutilities/hopsutilities.py
+++ localmodules/hopsutilities/hopsutilities.py
@@ -17,28 +17,19 @@ import Rhino # NOQA402
 
 # NUMPY & RHINO ---------------------------------------------------------------
 
+def rhino_points_to_np_array(points):
+    """
+    Converts a list of Rhino.Geometry.Point3d to a #Nx3 numpy array
+    """
+    return np.array([[pt.X, pt.Y, pt.Z] for pt in points])
+
+
 def rhino_mesh_to_np_arrays(mesh: Rhino.Geometry.Mesh):
     """
     Converts a Rhino.Geometry.Mesh to numpy arrays of vertices and faces.
     """
-    vertices = mesh.Vertices
-    faces = mesh.Faces
-    # V = np.array([[v.X,
-    #                vertices[i].Y,
-    #                vertices[i].Z] for i in range(vertices.Count)])
-
-    # F = np.array([[faces[i].A,
-    #                faces[i].B,
-    #                faces[i].C] for i in range(faces.Count)])
-
-    V = np.array([[v.X,
-                   v.Y,
-                   v.Z] for _, v in enumerate(mesh.Vertices)])
-
-    F = np.array([[f.A,
-                   f.B,
-                   f.C] for _, f in enumerate(mesh.Faces)])
-
+    V = np.array([[v.X, v.Y, v.Z] for _, v in enumerate(mesh.Vertices)])
+    F = np.array([[f.A, f.B, f.C] for _, f in enumerate(mesh.Faces)])
     return V, F
 
 
@@ -51,3 +42,10 @@ def np_array_to_rhino_transform(xform_np_array: np.array):
     for i, j in product(range(4), range(4)):
         rhino_xform[i, j] = xform_np_array[i][j]
     return rhino_xform
+
+
+def np_array_to_rhino_points(pt_np_array: np.array):
+    """
+    Converts a Nx3 numpy array to Rhino.Geometry.Point3d objects
+    """
+    return [Rhino.Geometry.Point3d(float(v[0]), float(v[1]), float(v[2])) for v in pt_np_array]
