b640c013473ad6be0ef15f2e6f35c5850965d7fb
fstwn <post@maxeschenbach.com>
2021-11-23 14:37:58 +0100

    add gh tree to np array method to init

--- src/malt/hopsutilities/__init__.py
+++ src/malt/hopsutilities/__init__.py
@@ -1,5 +1,6 @@
 from .hopsutilities import (rhino_points_to_np_array, # NOQA401
                             rhino_mesh_to_np_arrays,
+                            gh_tree_to_np_array,
                             np_array_to_rhino_transform,
                             np_array_to_rhino_points,
                             )
