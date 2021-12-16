af2f832ce4daca0fe54795c5a9b13150d02929c6
fstwn <mail@maxeschenbach.com>
2021-12-16 21:47:00 +0100

    fix bug in hsutil

--- src/malt/hopsutilities/__init__.py
+++ src/malt/hopsutilities/__init__.py
@@ -4,5 +4,5 @@ from .hopsutilities import (rhino_points_to_np_array, # NOQA401
                             np_array_to_rhino_transform,
                             np_array_to_rhino_points,
                             np_array_to_rhino_vectors,
-                            np_array_to_hops_tree,
+                            np_float_array_to_hops_tree,
                             )
