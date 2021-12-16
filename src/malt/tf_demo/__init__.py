24e8c45e8ccb2b2a4b13cac9b2ac9b27e2c3948c
fstwn <mail@maxeschenbach.com>
2021-12-16 21:24:51 +0100

    remove tf_demo

--- src/malt/tf_demo/__init__.py
+++
@@ -1,7 +0,0 @@
-# script by Yijiang Huang
-#
-# https://github.com/yijiangh/compas_rpc_examples
-# MIT license
-
-from .tf_match import (nn_forward_pass, # NOQA402
-                       get_model)
