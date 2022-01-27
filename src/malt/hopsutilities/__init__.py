5bd4a0b383b38462ce39028586271ce22aa1edf5
fstwn <mail@maxeschenbach.com>
2022-01-27 17:36:44 +0100

    add profiler to utils

--- src/malt/hopsutilities/__init__.py
+++ src/malt/hopsutilities/__init__.py
@@ -5,4 +5,7 @@ from .hopsutilities import (rhino_points_to_np_array, # NOQA401
                             np_array_to_rhino_points,
                             np_array_to_rhino_vectors,
                             np_float_array_to_hops_tree,
+                            np_int_array_to_hops_tree,
                             )
+
+from .Profiler import Profiler # NOQA401
