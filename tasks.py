28bf97f77c701119013fad86b6b7ed638030fbcb
fstwn <mail@maxeschenbach.com>
2022-02-02 19:56:15 +0100

    add gource task

--- tasks.py
+++ tasks.py
@@ -28,14 +28,14 @@ class Log(object):
 
     def write(self, message):
         self.flush()
-        self.out.write(message + '\n')
+        self.out.write("[STAT] %s" % message)
         self.out.flush()
 
     def info(self, message):
-        self.write('[INFO] %s' % message)
+        self.write("[INFO] %s" % message)
 
     def warn(self, message):
-        self.write('[WARN] %s' % message)
+        self.write("[WARN] %s" % message)
 
 
 log = Log()
@@ -57,8 +57,26 @@ def test(c):
     """
     Run all tests.
     """
+    log.write("Running all tests...")
     with chdir(malt.TESTDIR):
         c.run("pytest")
+
+
+@task()
+def gource(c):
+    """
+    Create gource video.
+    """
+    log.write("Creating gource visualization...")
+    with chdir(malt.REPODIR):
+        c.run(("gource {0} -1920x1080 -f --multi-sampling -a 1 -s 1 "
+               "--hide bloom,mouse,progress --camera-mode overview -r 60 -o "
+               "a1_s1_overview.ppm").format(malt.REPODIR))
+        log.write("Converting using FFMPEG...")
+        c.run("ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i "
+              "a1_s1_overview.ppm -vcodec libx264 -preset medium "
+              "-pix_fmt yuv420p -crf 1 -threads 0 -bf 0 a1_s1_overview.mp4")
+        os.remove("a1_s1_overview.ppm")
 
 
 # CONTEXT ---------------------------------------------------------------------
