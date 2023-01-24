1e42e516eaed6f23c3b0a6c4d206d62a7522b5e2
Max Eschenbach <eschenbach@dg.tu-darmstadt.de>
2023-01-24 15:54:31 +0100

    update init

--- src/malt/hopsutilities/__init__.py
+++ src/malt/hopsutilities/__init__.py
@@ -1,19 +1,20 @@
-from .hopsutilities import (sanitize_path, # NOQA401
-                            slash_join,
-                            validate_uuid,
-                            rhino_points_to_np_array,
-                            rhino_mesh_to_np_arrays,
-                            hops_path_to_tuple,
-                            hops_paths_to_tuples,
-                            hops_tree_to_np_array,
-                            hops_tree_verify,
-                            np_array_to_rhino_transform,
-                            np_array_to_rhino_points,
-                            np_array_to_rhino_vectors,
-                            np_float_array_to_hops_tree,
-                            np_int_array_to_hops_tree,
-                            np_arrays_to_rhino_triangle_mesh,
-                            nested_sequence_to_hops_tree,
-                            )
+from .hopsutilities import ( # NOQA401
+    sanitize_path,
+    slash_join,
+    validate_uuid,
+    rhino_points_to_np_array,
+    rhino_mesh_to_np_arrays,
+    hops_path_to_tuple,
+    hops_paths_to_tuples,
+    hops_tree_to_np_array,
+    hops_tree_verify,
+    np_array_to_rhino_transform,
+    np_array_to_rhino_points,
+    np_array_to_rhino_vectors,
+    np_float_array_to_hops_tree,
+    np_int_array_to_hops_tree,
+    np_arrays_to_rhino_triangle_mesh,
+    nested_sequence_to_hops_tree,
+)
 
 from .Profiler import Profiler # NOQA401
