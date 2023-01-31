aa2630de0dc1299768085cdd6d7ba9bc2a6a1715
Max Eschenbach <eschenbach@dg.tu-darmstadt.de>
2023-01-31 10:31:22 +0100

    first implementation of vsa module. does not work as expected and has to be revised

---
+++ src/malt/vsa/__init__.py
@@ -0,0 +1,16 @@
+# Implementation of the paper:
+#
+# Cohen-Steiner, D., Alliez, P., Desbrun, M., 2004.
+# Variational shape approximation.
+# ACM Trans. Graph. 23, 905–914.
+# https://doi.org/10.1145/1015706.1015817
+#
+# Original implementation by GitHub User romain22222
+# https://github.com/romain22222/PROJ602-Variational-shape-approximation
+#
+# Re-implemented for use with Rhino.Inside.CPython
+# by Max Eschenbach, DDU, TU-Darmstadt
+
+from .vsa import ( # NOQA401
+    VariationalSurfaceApproximation
+)
